ebb02bdcde00b7d1b94d562f0888762b53501f8a
lavana555 <timofeevniki3@gmail.com>
2020-03-17 19:52:56 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const app = express();
+const nodemailer = require("nodemailer");
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+app.use(cors())
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: "timofeevniki3@gmail.com", // generated ethereal user
+        pass: "studentik" // generated ethereal password
+    }
+});
+
+
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+
+
+app.post('/sendMessage',async function (req, res) {
+let {name,email,description}=req.body
+
+    let info = await transporter.sendMail({
+        from: 'nikita', // sender address
+        to: "timofeevniki3@gmail.com", // list of receivers
+        subject: "Hello ✔", // Subject line
+
+        html: `<b>messages with our portfolio</b> // html body
+        <div>
+            name:${name}
+        </div>
+<div>
+    email:${email}
+</div>
+<div>
+    description:${description}
+</div>`
+    });
+
+
+    // res.send('bla bla yo ');
+    res.send('ok');
+});
+
+
+app.listen(3010, function () {
+    console.log('Example app listening on port 3000!');
+});
